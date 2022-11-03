67c981cff870b81c9c4c89bd534940f2f2777ffe
fahrioghanial <fahrioghanial@gmail.com>
2022-11-03 21:47:50 +0700

    finish notification and guest

--- components/FormModal.js
+++ components/FormModal.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router"
 import moment from "moment";
 import "moment/locale/id";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { useState } from "react";
 
 export default function FormModal(props) {
@@ -11,11 +12,11 @@ export default function FormModal(props) {
       <label htmlFor="my-modal" className="btn btn-primary modal-button w-fit m-auto" onClick={props.resetEvent}>{props.label}</label>
       <input type="checkbox" id="my-modal" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box">
+        <div className="modal-box max-w-[768px]">
           <label htmlFor="my-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-          <form onSubmit={props.handleSubmit}>
+          <form onSubmit={props.handleSubmit} className="">
             <div className="flex md:flex-row flex-col gap-3">
-              <div>
+              <div className="md:w-1/2">
                 <div className="form-control w-full">
                   <label className="label">
                     <span className="label-text text-white">Judul Event</span>
@@ -30,7 +31,7 @@ export default function FormModal(props) {
                       value={props.title}
                       onChange={(e) => props.setTitle(e.target.value)}
                       placeholder="Judul Event"
-                      className="input input-bordered w-full max-w-xs"
+                      className="input input-bordered w-full"
                     />
                   }
                 </div>
@@ -48,7 +49,7 @@ export default function FormModal(props) {
                       value={props.description}
                       onChange={(e) => props.setDescription(e.target.value)}
                       placeholder="Deskripsi"
-                      className="input input-bordered w-full max-w-xs"
+                      className="input input-bordered w-full"
                     />
                   }
                 </div>
@@ -68,7 +69,7 @@ export default function FormModal(props) {
                         props.setStart(e.target.value)
                       }}
                       placeholder="Waktu Mulai"
-                      className="input input-bordered w-full max-w-xs"
+                      className="input input-bordered w-full"
                     />
                   }
                 </div>
@@ -86,7 +87,7 @@ export default function FormModal(props) {
                       value={props.end}
                       onChange={(e) => props.setEnd(e.target.value)}
                       placeholder="Waktu Selesai"
-                      className="input input-bordered bg- w-full max-w-xs"
+                      className="input input-bordered bg- w-full"
                     />
                   }
                 </div>
@@ -104,14 +105,53 @@ export default function FormModal(props) {
                       value={props.location}
                       onChange={(e) => props.setLocation(e.target.value)}
                       placeholder="Lokasi"
-                      className="input input-bordered w-full max-w-xs"
+                      className="input input-bordered w-full"
                     />
                   }
                 </div>
+                <div className={`${router.pathname == "/dashboard" ? "block" : "hidden"}`}>
+                  {!props.isFinished &&
+                    <>
+                      <div className="flex gap-3 items-center mt-3">
+                        <p>Notifikasi</p>
+                        <input
+                          type="number"
+                          value={props.notif}
+                          onChange={(e) => props.setNotif(e.target.value)}
+                          className="input input-bordered w-24 h-10"
+                        />
+                        <p>Menit</p>
+                      </div>
+                      <div className={`flex gap-3 items-center mt-3 ${props.isNotifEmailEnabled ? "block" : "hidden"}`}>
+                        <p>Email</p>
+                        <input
+                          type="number"
+                          value={props.notifEmail}
+                          onChange={(e) => props.setNotifEmail(e.target.value)}
+                          className="input input-bordered w-24 h-10"
+                        />
+                        <p>Menit</p>
+                      </div>
+                      <button className={`btn mt-3 bg-white text-black ${props.isNotifEmailEnabled ? "hidden" : "block"}`} onClick={props.addNotifEmail}>
+                        Tambahkan Notifikasi Email
+                      </button>
+                    </>
+                  }
+                  {props.isFinished &&
+                    <div className="ml-1">
+                      <div className="mt-3">
+                        Notifikasi: {props.notif} Menit
+                      </div>
+                      <div className={`${props.isNotifEmailEnabled ? "block" : "hidden"}`}>
+                        Notifikasi Email: {props.notifEmail} Menit
+                      </div>
+                    </div>
+                  }
+                </div>
               </div>
-              <div className={`form-control w-full ${router.pathname == "/dashboard" ? "block" : "hidden"}`}>
+              <div className={`form-control md:w-1/2 ${router.pathname == "/dashboard" ? "block" : "hidden"}`}>
                 <label className="label">
-                  <span className="label-text text-white">Tambahkan Tamu</span>
+                  <span className="label-text text-white">Tamu</span>
                 </label>
                 {props.isFinished &&
                   <>
@@ -125,23 +165,29 @@ export default function FormModal(props) {
                 }
                 {!props.isFinished &&
                   <>
-                    <input
-                      type="text"
-                      value={props.tempEmail}
-                      onChange={(e) => props.setTempEmail(e.target.value)}
-                      placeholder="emailtamu@contoh.com"
-                      className="input input-bordered w-full max-w-xs"
-                    />
-                    <button className="btn btn-primary" onClick={props.addGuests}>
-                      Tambah Tamu
-                    </button>
-                    {props.emailArray?.map(res => (
-                      <div key={res.email}>
-                        {res.email}
+                    <div className="relative">
+                      <input
+                        type="text"
+                        value={props.tempEmail}
+                        onChange={(e) => props.setTempEmail(e.target.value)}
+                        placeholder="emailtamu@contoh.com"
+                        className="input input-bordered w-full"
+                      />
+                      <button className="text-2xl flex items-center absolute right-3 top-0 bottom-0 hover:text-primary z-30" onClick={props.addGuests}>
+                        <i className="bi bi-plus-square-fill text-3xl"></i>
+                      </button>
+                    </div>
+                    {props.emailArray?.map((res, index) => (
+                      <div key={index} className="relative">
+                        <div className="hover:bg-slate-800 py-2 px-5">
+                          {res.email}
+                        </div>
+                        <button className="text-2xl flex items-center absolute right-3 top-0 bottom-0 hover:text-primary z-30" onClick={(e) => props.removeGuests(e, res.email)}>
+                          <i className="bi bi-x-square-fill text-3xl"></i>
+                        </button>
                       </div>
                     ))
                     }
-
                   </>
                 }
               </div>
@@ -155,11 +201,11 @@ export default function FormModal(props) {
                 Selesai
               </button>
               <div className={`${props.isFinished ? "block" : "hidden"}`}>
-                <button className={`btn btn-primary my-5`} type="submit">
+                <button className={`btn btn-primary my-5 mr-3`} type="submit">
                   Download File iCalendar (.ics)
                 </button>
                 {!props.eventHTMLLink &&
-                  <button className={`btn btn-primary my-5`} onClick={props.handleCreateEventGCalClick}>
+                  <button className={`btn btn-primary`} onClick={props.handleCreateEventGCalClick}>
                     Buat Event di Google Calendar
                   </button>
                 }
--- layouts/DashboardLayout.js
+++ layouts/DashboardLayout.js
@@ -1,38 +1,125 @@
 // components
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import FooterDashboard from "../components/Dashboard/FooterDashboard";
 import NavbarDashboard from "../components/Dashboard/NavbarDashboard";
 import Sidebar from "../components/Dashboard/Sidebar";
 
+// Google Cloud Project Client ID
+const CLIENT_ID = `${process.env.NEXT_PUBLIC_CLIENT_ID}`;
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
+
+let tokenClient;
 
 export default function DashboardLayout(props) {
   const router = useRouter();
+  const [userToken, setUserToken] = useState({});
+  const [userEmail, setUserEmail] = useState("");
+  const [userPicture, setUserPicture] = useState("");
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   function checkSession() {
+    // const token = gapi.client.getToken();
+    // if (token !== null) {
     if (localStorage.getItem('expiration') < Date.now()) {
+      localStorage.clear();
+      setUserToken({});
+      setUserEmail("");
+      setUserPicture("");
+      console.log('token expired');
       alert('Sesi Habis!, Silakan masuk kembali');
-      router.reload();
+      router.reload("/");
+    }
+    // }
+  }
+
+  function handleSignOut() {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+      console.log("Signed Out")
+      // google.accounts.oauth2.revoke(token.access_token);
+      gapi.client.setToken('');
+      localStorage.clear();
+      setUserToken({});
+      setUserEmail("");
+      setUserPicture("");
+      router.push("/");
     }
   }
 
+  function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+  }
+
+  async function initializeGapiClient() {
+    await gapi.client.init({
+      // apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+    if (localStorage.getItem('token') != null) {
+      gapi.client.setToken(JSON.parse(localStorage.getItem('token')));
+      console.log("token sent from localstorage:", gapi.client.getToken());
+    }
+  }
+
+  function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: CLIENT_ID,
+      scope: SCOPES,
+      callback: '', // defined later
+    });
+  }
+
+
+  useEffect(() => {
+    if (localStorage.getItem('expiration') != null) {
+      setTimeout(checkSession, (localStorage.getItem('expiration') - Date.now()))
+    }
+    if (localStorage.getItem('token') == null) {
+      router.push("/");
+    } else if (localStorage.getItem('expiration') > Date.now()) {
+      console.log('token still valid');
+      console.log('token time remaining (in second):', (localStorage.getItem('expiration') - Date.now()) / 1000)
+      // console.log("local storage:", localStorage.getItem('token'));
+      setUserToken(JSON.parse(localStorage.getItem('token')));
+      setUserEmail(localStorage.getItem('email'))
+      setUserPicture(localStorage.getItem('picture'))
+      setIsSignedIn(true);
+      gisLoaded();
+      gapiLoaded();
+    }
+  }, [])
+
   const title = `Evetra ${props.title != null ? " - " + props.title : ""}`
+
   return (
     <>
-      <Head>
-        <title>
-          {title}
-        </title>
-      </Head>
-      <div onClick={checkSession}>
-        <Sidebar />
-        <div data-theme="dark">
-          <NavbarDashboard userPicture={props.userPicture} handleSignOut={props.handleSignOut} userEmail={props.userEmail} />
-          <div className="mx-2 md:ml-80 pb-5 md:mr-5 text-white">
-            {props.children}
-            <FooterDashboard />
+      {isSignedIn && (
+        <>
+          <Head>
+            <title>
+              {title}
+            </title>
+          </Head>
+          <div>
+            <Sidebar />
+            <div data-theme="dark">
+              <NavbarDashboard userPicture={userPicture} handleSignOut={handleSignOut} />
+              <div className="mx-2 md:ml-80 pb-5 md:mr-5 text-white">
+                {props.children}
+                <FooterDashboard />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   )
 }
