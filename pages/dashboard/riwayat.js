ea28b97a116ad5e57645b776424a4f6f4edebf25
aaaabima <aaaabim@gmail.com>
2022-11-05 18:48:36 +0700

    Profile frontend without function and handling

---
+++ pages/dashboard/riwayat.js
@@ -0,0 +1,10 @@
+import Sidebar from "../../components/Dashboard/Sidebar";
+
+export default function riwayat() {
+    return (
+        <section>
+            <Sidebar/>
+            <p className="text-center">Riwayat</p>
+        </section>
+    )
+}
