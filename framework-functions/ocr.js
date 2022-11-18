7e934240640f5a988f179d3aef9f425c35788c5b
fahrioghanial <fahrioghanial@gmail.com>
2022-11-18 10:28:12 +0700

    add addblocker detection

--- framework-functions/ocr.js
+++ framework-functions/ocr.js
@@ -135,175 +135,178 @@ export const handleOCR = (e, OCRAttributes, setOCRAttributes, setEventAttributes
   // Send users PDF File to database
   formData.append('original_document', OCRAttributes.file);
   // Get users IPv4 address
-  axios.get('https://geolocation-db.com/json/').then(function (result) {
-    // Send users IPv4 address to database
-    formData.append('ip_address', result.data.IPv4);
-    if (localStorage.getItem("email") != null) {
-      formData.append('email', localStorage.getItem("email"));
-    }
-    const record = client.records.create('ocr_logging', formData);
-    record.then((result) => {
-      const collectionId = result["@collectionId"];
-      const recordId = result.id;
-      const filename = result.original_document;
-      const titleTemp = "";
-      const descriptionTemp = "";
-      const dateTemp = "";
-      const startTimeTemp = "";
-      const endTimeTemp = "";
 
-      // document direct download link for cognitive service
-      const formUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/files/${collectionId}/${recordId}/${filename}`;
-      console.log("collection id: ", collectionId);
-      console.log("record id: ", recordId);
-      console.log("filename: ", filename);
+  if (!window.canRunAds === undefined) {
+    axios.get('https://geolocation-db.com/json/').then(function (result) {
+      // Send users IPv4 address to database
+      formData.append('ip_address', result.data.IPv4);
+    })
+  }
+  if (localStorage.getItem("email") != null) {
+    formData.append('email', localStorage.getItem("email"));
+  }
+  const record = client.records.create('ocr_logging', formData);
+  record.then((result) => {
+    const collectionId = result["@collectionId"];
+    const recordId = result.id;
+    const filename = result.original_document;
+    const titleTemp = "";
+    const descriptionTemp = "";
+    const dateTemp = "";
+    const startTimeTemp = "";
+    const endTimeTemp = "";
 
-      async function main() {
-        const clientAzure = new DocumentAnalysisClient(endpoint, new AzureKeyCredential(key));
+    // document direct download link for cognitive service
+    const formUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/files/${collectionId}/${recordId}/${filename}`;
+    console.log("collection id: ", collectionId);
+    console.log("record id: ", recordId);
+    console.log("filename: ", filename);
 
-        const poller = await clientAzure.beginAnalyzeDocumentFromUrl("prebuilt-document", formUrl);
+    async function main() {
+      const clientAzure = new DocumentAnalysisClient(endpoint, new AzureKeyCredential(key));
 
-        const { keyValuePairs } = await poller.pollUntilDone();
+      const poller = await clientAzure.beginAnalyzeDocumentFromUrl("prebuilt-document", formUrl);
 
-        if (!keyValuePairs || keyValuePairs.length <= 0) {
-          console.log("No key-value pairs were extracted from the document.");
-        } else {
-          console.log("Key-Value Pairs:");
-          const formatted_data = [];
-          keyValuePairs.map((result, index) => {
-            formatted_data.push(formatter(result, index));
-          });
+      const { keyValuePairs } = await poller.pollUntilDone();
 
-          formatted_data.map((result) => {
-            if (/tema|acara|kegiatan/i.test(result.key)) {
-              console.log("key: ", result.key)
-              console.log("value: ", result.value)
-              titleTemp = result.value;
-              descriptionTemp = result.value;
+      if (!keyValuePairs || keyValuePairs.length <= 0) {
+        console.log("No key-value pairs were extracted from the document.");
+      } else {
+        console.log("Key-Value Pairs:");
+        const formatted_data = [];
+        keyValuePairs.map((result, index) => {
+          formatted_data.push(formatter(result, index));
+        });
+
+        formatted_data.map((result) => {
+          if (/tema|acara|kegiatan/i.test(result.key)) {
+            console.log("key: ", result.key)
+            console.log("value: ", result.value)
+            titleTemp = result.value;
+            descriptionTemp = result.value;
+          }
+          if (/hari|tanggal/i.test(result.key)) {
+            console.log("key: ", result.key)
+            console.log("value: ", result.value)
+            dateTemp = result.value;
+            if (dateTemp.includes(",")) {
+              dateTemp = dateTemp.substring(result.value.indexOf(",") + 1);
+            } else if (dateTemp.includes("/")) {
+              dateTemp = dateTemp.substring(result.value.indexOf("/") + 1);
+            } else if (dateTemp.includes(".")) {
+              dateTemp = dateTemp.substring(result.value.indexOf(".") + 1);
             }
-            if (/hari|tanggal/i.test(result.key)) {
-              console.log("key: ", result.key)
-              console.log("value: ", result.value)
-              dateTemp = result.value;
-              if (dateTemp.includes(",")) {
-                dateTemp = dateTemp.substring(result.value.indexOf(",") + 1);
-              } else if (dateTemp.includes("/")) {
-                dateTemp = dateTemp.substring(result.value.indexOf("/") + 1);
-              } else if (dateTemp.includes(".")) {
-                dateTemp = dateTemp.substring(result.value.indexOf(".") + 1);
-              }
-              if (dateTemp.indexOf(" ") == 0) {
-                dateTemp = dateTemp.substring(1);
-              }
-              dateTemp = dateTemp.split(/[" "]/);
-
-              if (dateTemp[0].length == 1) {
-                dateTemp[0] = "0" + dateTemp[0];
-              }
-              if (/Jan/i.test(dateTemp[1])) {
-                dateTemp[1] = "01";
-              }
-              else if (/Feb/i.test(dateTemp[1])) {
-                dateTemp[1] = "02"
-              }
-              else if (/Mar/i.test(dateTemp[1])) {
-                dateTemp[1] = "03"
-              }
-              else if (/Apr/i.test(dateTemp[1])) {
-                dateTemp[1] = "04"
-              }
-              else if (/Mei/i.test(dateTemp[1])) {
-                dateTemp[1] = "05"
-              }
-              else if (/Jun/i.test(dateTemp[1])) {
-                dateTemp[1] = "06"
-              }
-              else if (/Jul/i.test(dateTemp[1])) {
-                dateTemp[1] = "07"
-              }
-              else if (/Agu/i.test(dateTemp[1])) {
-                dateTemp[1] = "08"
-              }
-              else if (/Sep/i.test(dateTemp[1])) {
-                dateTemp[1] = "09"
-              }
-              else if (/Okt/i.test(dateTemp[1])) {
-                dateTemp[1] = "10"
-              }
-              else if (/Nov/i.test(dateTemp[1])) {
-                dateTemp[1] = "11"
-              }
-              else if (/Des/i.test(dateTemp[1])) {
-                dateTemp[1] = "12"
-              }
-
-              dateTemp = dateTemp.reverse();
-
-              console.log("dateTemp: ", dateTemp);
+            if (dateTemp.indexOf(" ") == 0) {
+              dateTemp = dateTemp.substring(1);
             }
+            dateTemp = dateTemp.split(/[" "]/);
 
-            if (/waktu|pukul/i.test(result.key)) {
-              console.log("key: ", result.key)
-              console.log("value: ", result.value)
-              startTimeTemp = result.value.substring(0, 5).split(/[.]/);
-              endTimeTemp = result.value.substring(result.value.length - 5);
-              if (endTimeTemp.includes(".")) {
-                endTimeTemp = endTimeTemp.split(/[.]/);
-              } else {
-                endTimeTemp = startTimeTemp.map(Number);
-                endTimeTemp[0] = endTimeTemp[0] + 1;
-                endTimeTemp = endTimeTemp.map(String);
-                if (endTimeTemp[0].length == 1) {
-                  endTimeTemp[0] = "0" + endTimeTemp[0];
-                }
-                if (endTimeTemp[1].length == 1) {
-                  endTimeTemp[1] = "0" + endTimeTemp[1];
-                }
-              }
-              console.log("start: ", startTimeTemp)
-              console.log("end: ", endTimeTemp)
-              setEventAttributes(eventAttributes => ({
-                ...eventAttributes,
-                start: dateTemp[0] + "-" + dateTemp[1] + "-" + dateTemp[2] + "T" + startTimeTemp[0] + ":" + startTimeTemp[1],
-                end: dateTemp[0] + "-" + dateTemp[1] + "-" + dateTemp[2] + "T" + endTimeTemp[0] + ":" + endTimeTemp[1]
-              }));
+            if (dateTemp[0].length == 1) {
+              dateTemp[0] = "0" + dateTemp[0];
+            }
+            if (/Jan/i.test(dateTemp[1])) {
+              dateTemp[1] = "01";
+            }
+            else if (/Feb/i.test(dateTemp[1])) {
+              dateTemp[1] = "02"
+            }
+            else if (/Mar/i.test(dateTemp[1])) {
+              dateTemp[1] = "03"
+            }
+            else if (/Apr/i.test(dateTemp[1])) {
+              dateTemp[1] = "04"
+            }
+            else if (/Mei/i.test(dateTemp[1])) {
+              dateTemp[1] = "05"
+            }
+            else if (/Jun/i.test(dateTemp[1])) {
+              dateTemp[1] = "06"
+            }
+            else if (/Jul/i.test(dateTemp[1])) {
+              dateTemp[1] = "07"
+            }
+            else if (/Agu/i.test(dateTemp[1])) {
+              dateTemp[1] = "08"
+            }
+            else if (/Sep/i.test(dateTemp[1])) {
+              dateTemp[1] = "09"
+            }
+            else if (/Okt/i.test(dateTemp[1])) {
+              dateTemp[1] = "10"
+            }
+            else if (/Nov/i.test(dateTemp[1])) {
+              dateTemp[1] = "11"
+            }
+            else if (/Des/i.test(dateTemp[1])) {
+              dateTemp[1] = "12"
             }
 
-            if (/tempat/i.test(result.key)) {
-              console.log("key: ", result.key)
-              console.log("value: ", result.value)
-              setEventAttributes(eventAttributes => ({
-                ...eventAttributes,
-                location: result.value
-              }));
-            }
-          })
+            dateTemp = dateTemp.reverse();
 
-          if (titleTemp == "" || descriptionTemp == "") {
+            console.log("dateTemp: ", dateTemp);
+          }
+
+          if (/waktu|pukul/i.test(result.key)) {
+            console.log("key: ", result.key)
+            console.log("value: ", result.value)
+            startTimeTemp = result.value.substring(0, 5).split(/[.]/);
+            endTimeTemp = result.value.substring(result.value.length - 5);
+            if (endTimeTemp.includes(".")) {
+              endTimeTemp = endTimeTemp.split(/[.]/);
+            } else {
+              endTimeTemp = startTimeTemp.map(Number);
+              endTimeTemp[0] = endTimeTemp[0] + 1;
+              endTimeTemp = endTimeTemp.map(String);
+              if (endTimeTemp[0].length == 1) {
+                endTimeTemp[0] = "0" + endTimeTemp[0];
+              }
+              if (endTimeTemp[1].length == 1) {
+                endTimeTemp[1] = "0" + endTimeTemp[1];
+              }
+            }
+            console.log("start: ", startTimeTemp)
+            console.log("end: ", endTimeTemp)
             setEventAttributes(eventAttributes => ({
               ...eventAttributes,
-              title: OCRAttributes.file.name.replace(".pdf", ""),
-              description: OCRAttributes.file.name.replace(".pdf", "")
-            }));
-          } else {
-            setEventAttributes(eventAttributes => ({
-              ...eventAttributes,
-              title: titleTemp,
-              description: descriptionTemp
+              start: dateTemp[0] + "-" + dateTemp[1] + "-" + dateTemp[2] + "T" + startTimeTemp[0] + ":" + startTimeTemp[1],
+              end: dateTemp[0] + "-" + dateTemp[1] + "-" + dateTemp[2] + "T" + endTimeTemp[0] + ":" + endTimeTemp[1]
             }));
           }
 
-          setOCRAttributes(OCRAttributes => ({
-            ...OCRAttributes,
-            isOCRFinished: true,
+          if (/tempat/i.test(result.key)) {
+            console.log("key: ", result.key)
+            console.log("value: ", result.value)
+            setEventAttributes(eventAttributes => ({
+              ...eventAttributes,
+              location: result.value
+            }));
+          }
+        })
+
+        if (titleTemp == "" || descriptionTemp == "") {
+          setEventAttributes(eventAttributes => ({
+            ...eventAttributes,
+            title: OCRAttributes.file.name.replace(".pdf", ""),
+            description: OCRAttributes.file.name.replace(".pdf", "")
+          }));
+        } else {
+          setEventAttributes(eventAttributes => ({
+            ...eventAttributes,
+            title: titleTemp,
+            description: descriptionTemp
           }));
         }
 
+        setOCRAttributes(OCRAttributes => ({
+          ...OCRAttributes,
+          isOCRFinished: true,
+        }));
       }
 
-      main().catch((error) => {
-        console.error("An error occurred:", error);
-      });
-    })
+    }
+
+    main().catch((error) => {
+      console.error("An error occurred:", error);
+    });
   })
 };
